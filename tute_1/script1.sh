3140c59d093e74927a5ae00e1a0522fdd32c55bc
Mundane-Editorial <uday2k17@rediffmail.com>
2024-10-04 15:22:34 +0530

    hello wolrd : bash

--- tute_1/script1.sh
+++ tute_1/script1.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/bash  
 
 echo "Hello, Linux"
+
+echo "My current working directory is"
+pwd
+
+echo "Current logged-in users:"
+whoami
+
+echo "Number of users currently logged in:"
+who | wc -l
+
+echo "Disk usage:"
+df -h
+
